738af8d485c55585ef57d9ea214231fe7d4caa76
Sander Ulset <sanderulset1@gmail.com>
2021-05-08 01:45:10 +0200

    Ser hvor mange man har vunnet

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ rw = RouletteWrapper(log_in=False)
 
 # Fake money to start with
 start_money = 1000
-MAX_LOSS_IN_ROW = 5
+MAX_LOSS_IN_ROW = 7
 
 
 def calculate_bet(total_money, max_loss):
@@ -15,10 +15,14 @@ money = start_money
 bet = calculate_bet(money, MAX_LOSS_IN_ROW)
 chance_of_failure = 0.5 ** MAX_LOSS_IN_ROW
 print(f"Startet med {money} i kapital. Max antall tap før konkurs: {MAX_LOSS_IN_ROW}({chance_of_failure}% sjanse)")
+
+games_played = 0
+games_won = 0
 while True:
     if money < 0:
         raise ValueError("Alle pengene tapt")
 
+    games_played += 1
     while float(rw.get_timer()) > 1.5:
         pass
 
@@ -31,8 +35,10 @@ while True:
         print("Vant")
         money += bet
         bet = calculate_bet(money, MAX_LOSS_IN_ROW)
+        games_won += 1
     else:
         print("Tapte")
         money -= bet
         bet = bet * 2
-    print(f"Penger nå: {money}\n")
+    print(f"Penger nå: {money}")
+    print(f"Vunnet {games_won} av {games_played} ({round((games_won/games_played)*100, 2)} %)\n")
